7150876d54d68047fc16e5237cfb9691cdf6ae85
Varpik <162813409+Abama202@users.noreply.github.com>
2026-09-07 19:37:57 +0300

    Update Untitled.py

--- Untitled.py
+++ Untitled.py
@@ -1,16 +1,25 @@
 print("Виберіть")
-print("1 - Додавання, 2-Віднімання, 3 - множення")
+
+print("1 - Додавання, 2 - Віднімання, 3 - множення")
 
 Vibir = input("Введіть номер: ")
 
-N1 = input("Введіть перше число: ")
-N2 = input("Введіть друге число: ")
+
+num1 = input("Введіть перше число: ")
+
+num2 = input("Введіть друге число: ")
+
+
 
 if Vibir == "1":
-    print("Результат: ", float(N1) + float(N2))
+    print("Результат: ", float(num1) + float(num2))
+
 elif Vibir == "2":
-    print("Результат: ", float(N1) - float(N2))
+    print("Результат: ", float(num1) - float(num2))
+
 elif Vibir == "3":
-    print("Результат: ", float(N1) * float(N2))
+    print("Результат: ", float(num1) * float(num2))
+
 else:
     print("Невірний вибір")
+    print("Спробуйте ще раз")
